43a604e76e2ebb25143d9df0453b3fd943824fd0
pavel_sol <59879066+Pavel-Sol@users.noreply.github.com>
2021-05-24 00:18:51 +0400

    взимодействие с бд, сохранение/вывод сообщений

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -4,16 +4,33 @@ import {useAuthState} from 'react-firebase-hooks/auth'
 import {useContext, useState} from 'react'
 
 import {Context} from './../index'
+import Loader from './Loader'
 
 
 const Chat = () => {
 
-   const {auth, firestore} = useContext(Context) 
+   const {auth, firestore, firebase} = useContext(Context);
    const [user, loading, error] = useAuthState(auth);
-   const [value, setValue] = useState('')
+   const [value, setValue] = useState('');
 
-   const sendMessage = () => {
-      console.log(value)
+   const [messages, loadingMsg] = useCollectionData(
+      firestore.collection('messages').orderBy('createdAt')
+   )
+
+   const sendMessage = async () => {
+      // console.log(value)
+      firestore.collection('messages').add({
+         uid: user.uid,
+         text: value,
+         displayName: user.displayName,
+         photoURL: user.photoURL,
+         createdAt: firebase.firestore.FieldValue.serverTimestamp()
+      })
+      setValue('')
+   }
+
+   if(loadingMsg) {
+      return <Loader/>
    }
 
    return(
@@ -21,6 +38,10 @@ const Chat = () => {
          <div className="chat__wrapper">
             <div className="messages__wrapper">
                <div className="messages">
+                  {
+                     messages.map((message) =>
+                        <div>{message.text}</div>)
+                  }
                </div>
             </div>
             <div className="form__wrapper">
